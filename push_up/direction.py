1d7ab1ae81e9a0a196ec1575af5bc2f0f4bc059a
JaiyoungJoo <wodudwn@daum.net>
2022-12-02 13:55:26 +0900

    fix:imList[28][1] -> lmList[24][1]

--- push_up/direction.py
+++ push_up/direction.py
@@ -5,21 +5,20 @@ import math
 
 # check right or left direction
 
-def direction(img, lmList):
-    
+def direction(img, lmList, view):
+    view = ''
     if len(lmList) != 0 :
-        if lmList[12][1] > lmList[28][1]:
-            cv2.putText(img, str('right'), (100, 100), cv2.FONT_HERSHEY_PLAIN, 3,
+        if lmList[12][1] > lmList[24][1]:
+            cv2.putText(img, str('right view'), (20, 100), cv2.FONT_HERSHEY_PLAIN, 3,
                     (255, 0, 0), 3)
-            
-            
-        else:
-            cv2.putText(img, str('left'), (100, 100), cv2.FONT_HERSHEY_PLAIN, 3,
-                    (255, 0, 0), 3)
-            
-            
+            view = 'right'
 
-    return img
+        else:
+            cv2.putText(img, str('left view'), (20, 100), cv2.FONT_HERSHEY_PLAIN, 3,
+                    (255, 0, 0), 3)
+            view = 'left'
+            
+    return img, view
 
 
 
