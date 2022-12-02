6ecc5fc05106dca9b592764c8502108124f00378
JaiyoungJoo <wodudwn@daum.net>
2022-12-02 14:40:28 +0900

    fix:input value armdeg, bodyde

--- push_up/degree.py
+++ push_up/degree.py
@@ -5,33 +5,56 @@ import math
 
 # right
 # 점14를 기준으로 (12 - 14) (16 - 14) 이렇게 하면 벡터 2개가 나옴.
-def degree(img, lmList, width, height, view):
+def degree(img, lmList, width, height, view, armdeg, bodydeg):
     
     if len(lmList) != 0 :
         if view == 'right':
-            # 벡터 구하기
-            rightarm_v1 = (lmList[12][1]-lmList[14][1], lmList[12][2]-lmList[14][2])
-            rightarm_v2 = (lmList[16][1]-lmList[14][1], lmList[16][2]-lmList[14][2])
+            # ===rightarm 12번, 14번, 16번===
             # 각 벡터 길이 구하기
-            d1 = math.sqrt((lmList[12][1]-lmList[14][1])**2 + (lmList[12][2]-lmList[14][2])**2)
-            d2 = math.sqrt((lmList[16][1]-lmList[14][1])**2 + (lmList[16][2]-lmList[14][2])**2)
+            rightarm_d1 = math.sqrt((lmList[12][1]-lmList[14][1])**2 + (lmList[12][2]-lmList[14][2])**2)
+            rightarm_d2 = math.sqrt((lmList[16][1]-lmList[14][1])**2 + (lmList[16][2]-lmList[14][2])**2)
             # 두 벡터 사이 점곱
             v1v2 = (lmList[12][1]-lmList[14][1])*(lmList[16][1]-lmList[14][1]) + (lmList[12][2]-lmList[14][2])*(lmList[16][2]-lmList[14][2])
 
             # cos값 구하기
-            c = v1v2/(d1*d2)
-
+            arm_c = v1v2/(rightarm_d1*rightarm_d2)
             # cos값으로 radian값 구하기
-            rad = math.acos(c)
+            arm_rad = math.acos(arm_c)
+            PI = math.pi
+            arm_deg = (arm_rad*180)/PI
+            arm_deg = int(arm_deg)
+
+            # 영상 우상단에 text
+            cv2.putText(img, f'arm{int(arm_deg):3d}', (int(width-400), 50), cv2.FONT_HERSHEY_PLAIN, 3,
+                        (255, 0, 0), 3)
+            cv2.putText(img, 'degree', (int(width-200), 50), cv2.FONT_HERSHEY_PLAIN, 3,
+                        (255, 0, 0), 3)
+
+            # ===rightbody 12번, 24번, 26번===
+            # 각 벡터 길이 구하기
+            body_d1 = math.sqrt((lmList[12][1]-lmList[24][1])**2 + (lmList[12][2]-lmList[24][2])**2)
+            body_d2 = math.sqrt((lmList[26][1]-lmList[24][1])**2 + (lmList[26][2]-lmList[24][2])**2)
+            # 두 벡터 사이 점곱
+            bodyv1v2 = (lmList[12][1]-lmList[24][1])*(lmList[26][1]-lmList[24][1]) + (lmList[12][2]-lmList[24][2])*(lmList[26][2]-lmList[24][2])
+
+            # cos값 구하기
+            body_c = bodyv1v2/(body_d1*body_d2)
+            # cos값으로 radian값 구하기
+            rad = math.acos(body_c)
             PI = math.pi
             deg = (rad*180)/PI
             deg = int(deg)
 
             # 영상 우상단에 text
-            cv2.putText(img, 'rightarm'+str(deg), (int(width-400), 50), cv2.FONT_HERSHEY_PLAIN, 3,
+            cv2.putText(img, f'body{int(deg):3d}', (int(width-430), 100), cv2.FONT_HERSHEY_PLAIN, 3,
                         (255, 0, 0), 3)
-            cv2.putText(img, 'degree', (int(width-200), 50), cv2.FONT_HERSHEY_PLAIN, 3,
+            cv2.putText(img, 'degree', (int(width-200), 100), cv2.FONT_HERSHEY_PLAIN, 3,
                         (255, 0, 0), 3)
+
+            armdeg = arm_deg
+            bodydeg = deg
+
+
 
         elif view == 'left':
             # 벡터 구하기
@@ -50,11 +73,35 @@ def degree(img, lmList, width, height, view):
             rad = math.acos(c)
             PI = math.pi
             deg = (rad*180)/PI
-            deg = int(deg)
+            arm_deg = int(deg)
 
             # 영상 우상단에 text
-            cv2.putText(img, 'leftarm'+str(deg), (int(width-400), 50), cv2.FONT_HERSHEY_PLAIN, 3,
+            cv2.putText(img, 'arm'+str(arm_deg), (int(width-400), 50), cv2.FONT_HERSHEY_PLAIN, 3,
                         (255, 0, 0), 3)
             cv2.putText(img, 'degree', (int(width-200), 50), cv2.FONT_HERSHEY_PLAIN, 3,
                         (255, 0, 0), 3)
-    return img
+
+            # ===leftbody 11번, 23번, 25번===
+            # 각 벡터 길이 구하기
+            body_d1 = math.sqrt((lmList[11][1]-lmList[23][1])**2 + (lmList[11][2]-lmList[23][2])**2)
+            body_d2 = math.sqrt((lmList[25][1]-lmList[23][1])**2 + (lmList[25][2]-lmList[23][2])**2)
+            # 두 벡터 사이 점곱
+            bodyv1v2 = (lmList[11][1]-lmList[23][1])*(lmList[25][1]-lmList[23][1]) + (lmList[11][2]-lmList[23][2])*(lmList[25][2]-lmList[23][2])
+
+            # cos값 구하기
+            body_c = bodyv1v2/(body_d1*body_d2)
+            # cos값으로 radian값 구하기
+            rad = math.acos(body_c)
+            PI = math.pi
+            deg = (rad*180)/PI
+            deg = int(deg)
+
+            # 영상 우상단에 text
+            cv2.putText(img, f'body{int(deg):3d}', (int(width-430), 100), cv2.FONT_HERSHEY_PLAIN, 3,
+                        (255, 0, 0), 3)
+            cv2.putText(img, 'degree', (int(width-200), 100), cv2.FONT_HERSHEY_PLAIN, 3,
+                        (255, 0, 0), 3)
+            
+            armdeg = arm_deg
+            bodydeg = deg
+    return img, armdeg, bodydeg
--- push_up/testprac.py
+++ push_up/testprac.py
@@ -3,17 +3,24 @@ import time
 import PoseModule2 as pm
 import direction 
 import degree
+# import score
 
 # 함수 main에 있는 부분 그대로 복붙
 
 cap = cv2.VideoCapture('perfect_push_up.mp4')
 # cap = cv2.VideoCapture('leftpushup.mp4')
+# cap = cv2.VideoCapture('rightpushup.mp4')
 # cap = cv2.VideoCapture('only_federer.avi')
+
 pTime = 0
 
 # 이거 pm. 해야 함.
 
 detector = pm.poseDetector()
+
+width = cap.get(cv2.CAP_PROP_FRAME_WIDTH)
+height = cap.get(cv2.CAP_PROP_FRAME_HEIGHT)
+
 cnt = 0
 while True:
     cnt+=1
@@ -23,26 +30,29 @@ while True:
         break
     img = detector.findPose(img)
     lmList = detector.findPosition(img, draw=False)
+    # lmList = detector.findPosition(img, draw=True)
     if len(lmList) !=0:
         # print(lmList)
         print('========================')
-        cv2.circle(img, (lmList[14][1], lmList[14][2]), 15, (0, 0, 255), cv2.FILLED)
+        # cv2.circle(img, (lmList[14][1], lmList[14][2]), 15, (0, 0, 255), cv2.FILLED)
 
     cTime = time.time()
     fps = 1 / (cTime - pTime)
     pTime = cTime
 
-    # cv2.putText(img, str(int(fps)), (70, 50), cv2.FONT_HERSHEY_PLAIN, 3,
-    #             (255, 0, 0), 3)
+    cv2.putText(img, str(int(fps)), (20, 50), cv2.FONT_HERSHEY_PLAIN, 3,
+                (255, 0, 0), 3)
     # cv2.putText(img, str(int(cnt)), (200, 100), cv2.FONT_HERSHEY_PLAIN, 3,
     #             (255, 0, 0), 3)
 
     # cv2.imshow("Image", img)
     # cv2.waitKey(1)
     # img2 = img[200:, 100:600]
-    
-    img = direction.direction(img, lmList)
-    img = degree.degree(img, lmList)
+    view=''
+    armdeg = 0
+    bodydeg = 0
+    img, view = direction.direction(img, lmList, view)
+    img, armdeg, bodydeg = degree.degree(img, lmList, width, height, view, armdeg, bodydeg)
     
     cv2.imshow("Image", img)
     if cv2.waitKey(10) == 27:
