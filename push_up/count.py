4782a8e6bb87118a8239883efa9c006f9a74495f
JaiyoungJoo <wodudwn@daum.net>
2022-12-13 20:07:59 +0900

    feat:Make a count function

--- push_up/count.py
+++ push_up/count.py
@@ -1,10 +1,14 @@
 import cv2
 
+'''
+count 기준
+- 팔이 100도 보다 밑으로 내려갈 것
+- 팔이 160도보다 펴질 것
+'''
 def count(img, armdeg, bodydeg, stack, num):
-    # if stack == -1 and armdeg <= 90:
-    #     stack = 0
 
-    if armdeg <= 90 and stack == 0:
+
+    if armdeg <= 100 and stack == 0:
         stack = 1
     elif armdeg >= 160 and stack == 1:
         num += 1
@@ -12,7 +16,7 @@ def count(img, armdeg, bodydeg, stack, num):
 
         
         
-    cv2.putText(img, f'count:{num}+stack:{stack}', (20, 150), cv2.FONT_HERSHEY_PLAIN, 3,
+    cv2.putText(img, f'count:{num}, stack:{stack}', (20, 150), cv2.FONT_HERSHEY_PLAIN, 3,
                 (255, 0, 0), 3)
 
     return img, num, stack
